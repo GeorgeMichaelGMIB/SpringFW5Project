9635b09f497b8ffb0056722b039c18ef6b736197
GeorgeMichaelGMIB <76493623+GeorgeMichaelGMIB@users.noreply.github.com>
2021-09-26 04:00:42 +0200

    adding ToString and Equals methods for both POJOS

--- home/model/Author.java
+++ home/model/Author.java
@@ -8,4 +8,19 @@ public class Author{
 	private String middleName;
 	@ManyToMany(mappedBy="authors")
 	private ArrayList<Book> books;
+	
+	
+	@Override
+	public boolean equals(Object o) {
+		if(this == o)return true;
+		if(o== null | getClass() != o.getClass()) return false;
+		Author author = (Author) new Author();
+		return id !=null? id.equals(author.id) : author.id==null;
+	}
+	
+	@Override
+	public int hashCode() {
+		return id.hashCode() != null id.hashCode():0;
+	}
+	
 }
